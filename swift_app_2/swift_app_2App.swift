10484e96afafbf8b7d842b9e06a62898303ccfc2
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2024-01-01 19:54:19 +0700

    Initial Commit

---
+++ swift_app_2/swift_app_2App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_app_2App.swift
+//  swift_app_2
+//
+//  Created by ReviseUC73 on 1/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct swift_app_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
